90c633dec2cae3effd83800226d9650419cf4423
BeginnerDualist <74245444+BeginnerDualist@users.noreply.github.com>
2024-04-29 11:44:12 +0300

    image_imshow

--- torchvision.py
+++ torchvision.py
@@ -1,1 +1,8 @@
 import torch
+import matplotlib.pyplot as plt
+import os
+path='C:\\Users\\rast1\\Downloads\\dataset\\sdf.png'
+from PIL import Image
+img=Image.open(path)
+plt.imshow(img)
+plt.show()
