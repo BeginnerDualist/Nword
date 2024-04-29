2ee2add5213d4c1ff289ca460f07fe93fd34321b
BeginnerDualist <74245444+BeginnerDualist@users.noreply.github.com>
2024-04-29 14:04:04 +0300

    evolution

--- torchvision.py
+++ torchvision.py
@@ -1,10 +1,19 @@
 import torch
 import matplotlib.pyplot as plt
 import os
-path='C:\\Users\\rast1\\Downloads\\dataset\\sdf.png'
+import numpy as np
 from PIL import Image
-img=Image.open(path)
+
+path = 'c:\\users\\rast1\\downloads\\dataset\\sdf.png'
+img = Image.open(path)
 plt.imshow(img)
 plt.show()
-import numpy as np
-print(np.arange(1,10,2))
+print(np.arange(1, 10, 2))
+#git revert делает новый коммит коми
+#git reset удаляет историю где в команде указывается hash коммита до которого мы хотим удалить
+# Отмена всех изменений:
+# Если вы хотите отменить все изменения в рабочей директории и вернуться к последнему коммиту,
+# выполните следующую команду:
+# css
+# Copy code
+# git reset --hard HEAD
