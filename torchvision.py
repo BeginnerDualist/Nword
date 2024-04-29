670c99d61b518f3d8584b5b3067d099968782b83
BeginnerDualist <74245444+BeginnerDualist@users.noreply.github.com>
2024-04-29 14:21:03 +0300

    Revert "evolution"
    This reverts commit 2ee2add5213d4c1ff289ca460f07fe93fd34321b.

--- torchvision.py
+++ torchvision.py
@@ -1,19 +1,10 @@
 import torch
 import matplotlib.pyplot as plt
 import os
-import numpy as np
+path='C:\\Users\\rast1\\Downloads\\dataset\\sdf.png'
 from PIL import Image
-
-path = 'c:\\users\\rast1\\downloads\\dataset\\sdf.png'
-img = Image.open(path)
+img=Image.open(path)
 plt.imshow(img)
 plt.show()
-print(np.arange(1, 10, 2))
-#git revert делает новый коммит коми
-#git reset удаляет историю где в команде указывается hash коммита до которого мы хотим удалить
-# Отмена всех изменений:
-# Если вы хотите отменить все изменения в рабочей директории и вернуться к последнему коммиту,
-# выполните следующую команду:
-# css
-# Copy code
-# git reset --hard HEAD
+import numpy as np
+print(np.arange(1,10,2))
